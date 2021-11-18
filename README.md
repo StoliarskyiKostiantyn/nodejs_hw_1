2374d6ed6b73187a2d43d578ed13cf901201d7cd
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2021-11-18 20:55:09 +0200

    commit finish

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodejs_hw_1
- 
+
+https://ibb.co/ydCrzSp
+https://ibb.co/tsxkmgq
+https://ibb.co/7QFBC4Z
+https://ibb.co/y6WxrKh
--- contacts.js
+++ contacts.js
@@ -12,7 +12,7 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   const contacts = await listContacts();
-  const result = contacts.find((item) => item.id === contactId);
+  const result = contacts.find((item) => item.id === parseInt(contactId));
   console.log(result);
   if (!result) {
     return null;
@@ -22,7 +22,7 @@ async function getContactById(contactId) {
 
 async function removeContact(contactId) {
   const contacts = await listContacts();
-  const idx = contacts.findIndex((item) => item.id === contactId);
+  const idx = contacts.findIndex((item) => item.id === parseInt(contactId));
   if (idx === -1) {
     return null;
   }
@@ -36,6 +36,7 @@ async function addContact(name, email, phone) {
   const newContact = { id: v4(), name: name, email: email, phone: phone };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
+  return newContact;
 }
 
 module.exports = {
